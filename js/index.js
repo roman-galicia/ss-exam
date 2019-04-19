a9447a57077d08a3f486bfc9978197e353c47317
Roman <rchornopyskyi@gmail.com>
2019-04-19 08:54:58 +0000

    update all document

---
+++ js/index.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+    document.querySelector('.clock').innerText = (new Date()).toLocaleTimeString(), 1000
+});
+
+document.querySelector('.login-form').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    fetch('form.php', {
+            method: 'POST',
+            body: new FormData(document.querySelector('.login-form'))
+        })
+        .then(response => response.text())
+        .then(html => document.querySelector('.server-response')
+            .innerHTML = html);
+}
